c3e4c1c23b2d80c93dd8989e52b8f17ff7df2ce3
Fredrik Sandström <Fredrik.Sandstrom@triona.se>
2017-01-08 16:46:06 +0100

    Added models

---
+++ Music Player/App/Models/Player.ts
@@ -0,0 +1,20 @@
+﻿module App.Models {
+
+  export interface IPlayer {
+    allTracks: Array<ITrack>;
+    playList: Array<ITrack>;
+    temporarySearchPlaylist: Array<ITrack>;
+    lastPlayed: Array<ITrack>;
+    currentTrack: number;
+    shuffle: boolean;
+    repeat: number;
+
+  }
+
+  export interface ITrack {
+    isPlaying: boolean;
+    picture: any;
+    title: string;
+    artist: string;
+  }
+}
